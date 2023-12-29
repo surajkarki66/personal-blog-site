21bc773b4245342d5aaebb0f6b0984c977539c5e
surajkarki <suraj.karki500@protonmail.com>
2023-12-29 20:26:11 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ echo "Building the project..."
 python3 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 
 echo "Collect Static..."
