c36535fd32969ed36547cdb95ac27c2c2cda838e
surajkarki <suraj.karki500@protonmail.com>
2023-12-30 10:42:18 +0100

    updated vercel config

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 # Build the project
 echo "Building the project..."
-python3.11.5 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.11.5 manage.py makemigrations --noinput
-python3.11.5 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.11.5 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
