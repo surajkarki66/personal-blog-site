9ac9abc03f16acbdad36ba78ff3d5e88799d55e4
surajkarki <suraj.karki500@protonmail.com>
2023-12-30 11:13:46 +0100

    added postgres config

--- blog/settings.py
+++ blog/settings.py
@@ -88,9 +88,11 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str('POSTGRES_DATABASE'),
+        'USER': env.str('POSTGRES_USER'),
+        'PASSWORD': env.str('POSTGRES_PASSWORD'),
+        'HOST': env.str('POSTGRES_HOST'),
     }
 }
 
